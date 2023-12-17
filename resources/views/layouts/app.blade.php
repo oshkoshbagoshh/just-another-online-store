f1cc9b86bc6db3b62634a9e66ce27eadbfec6359
AJ <amirjavadi25@gmail.com>
2023-12-16 22:06:25 -0600

    working on bootstrap 5.1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name = "viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          crossorigin="anonymous" />
+    <title> @yield('title', 'Online Store') </title>
+</head>
+
+<body>
+
+<!--- header --->
+
+
+</body>
+
+</html>
