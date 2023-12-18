684bc9a09d0e915102397f70709de2214eff34d4
AJ <amirjavadi25@gmail.com>
2023-12-17 23:20:28 -0600

    refactored  home route

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-
-    $viewData = [];
-    $viewData["title"] = "Home - Just Another Online Store";
-    return view('home.index')->with("viewData", $viewData);
-
-});
+// home / root route
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
 
