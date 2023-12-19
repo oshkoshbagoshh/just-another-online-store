75f5f213df5300b60be51f00efaeb0f387bffd52
AJ <amirjavadi25@gmail.com>
2023-12-18 19:25:56 -0600

    updated title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
     <header class="masthead bg-primary text-white text-center py-4">
         <div class="container d-flex align-items-center flex-column">
-            <h2> @yield('subtitle', 'A Laravel Online Store') </h2>
+            <h2> @yield('subtitle', 'Just Another Online Store') </h2>
         </div>
 
     </header>
