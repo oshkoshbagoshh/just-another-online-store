440faf4544cb6e79a3a1f825c9b00a56084e47af
AJ <amirjavadi25@gmail.com>
2023-12-19 19:50:30 -0600

    changed product php and updated notes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory; //TODO: add in a factory with fake data later 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+/*
+ * @Author: AJ Javadi
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2023-12-19 19:29:45
+ * @Last Modified by:   Someone
+ * @Last Modified time: 2023-12-19 19:38:05
+ * @Description: file:///Users/aj/sandbox/just-another-online-store/app/Models/Product.php
+ *
+ * PRODUCT ATTRICUTES
+ * $this->attributes['id'] - int - contains the product primary key (id)
+ * $this->attributes['name'] - string - contains the product name
+ * $this->attributes['description'] - string - contains the product description
+ * $this->attributes['image'] - string - contains the product image
+ * $this->attributes['price'] - int - contains the product price
+ * $this->attributes['created_at'] - timestamp - contains the product creation date
+ * $this->attributes['updated_at'] - timestamp - contains the product updated date
+
+
+
+ */
+
 }
