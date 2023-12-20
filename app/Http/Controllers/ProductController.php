9cd4105bfab076eb0f1f8568115f7f567a7fc4bf
AJ <amirjavadi25@gmail.com>
2023-12-20 04:42:02 -0600

    added functionality to let users upload their own images

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * @Author: AJ Javadi
- * @Email: amirjavadi25@gmail.com
- * @Date: 2023-12-19 12:05:04
- * @Last Modified by:   Someone
- * @Last Modified time: 2023-12-19 20:27:06
- * @Description: file:///Users/aj/sandbox/just-another-online-store/app/Http/Controllers/ProductController.php
- */
 
 namespace App\Http\Controllers;
 
@@ -19,26 +11,19 @@ class ProductController extends Controller
     public function index()
     {
         $viewData = [];
-        $viewData['title'] = "Products - Just Another Online Store";
-        $viewData['subtitle'] = "List of products";
-        $viewData['products'] = Product::all();
+        $viewData["title"] = "Products - Online Store";
+        $viewData["subtitle"] =  "List of products";
+        $viewData["products"] = Product::all();
         return view('product.index')->with("viewData", $viewData);
     }
 
-    public function show($id) {
-
+    public function show($id)
+    {
         $viewData = [];
         $product = Product::findOrFail($id);
-        // $viewData['title'] = $product["name"]." - Just Another Online Store";
-        $viewData["title"] = $product->getName(). " - Just Another Online Store";
-        // $viewData['subtitle'] = $product["name"]." - Product Information";
-        $viewData["subtitle"] = $product->getName()." - Product Information";
-        $viewData["product"]= $product;
-        return view("product.show")->with("viewData", $viewData);
-
+        $viewData["title"] = $product->getName()." - Online Store";
+        $viewData["subtitle"] =  $product->getName()." - Product information";
+        $viewData["product"] = $product;
+        return view('product.show')->with("viewData", $viewData);
     }
-
-
-
-// -------------------- FIN
 }
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,63 +1,65 @@
 @extends('layouts.admin')
 @section('title', $viewData['title'])
 @section('content')
-    {{-- includes --}}
-    {{-- =================== --}}
-
-    {{-- Create Products --}}
     <div class="card mb-4">
         <div class="card-header">
             Create Products
         </div>
         <div class="card-body">
             @if ($errors->any())
-                <!-- Corrected: Removed extra parenthesis -->
                 <ul class="alert alert-danger list-unstyled">
                     @foreach ($errors->all() as $error)
-                        <li> - {{ $error }}</li>
+                        <li>- {{ $error }}</li>
                     @endforeach
                 </ul>
             @endif
 
-            {{-- POST METHOD FORM --}}
-            <!-- Corrected: Added missing quote -->
-            <form method="POST" action="{{ route('admin.products.store') }}">
+            <form method="POST" action="{{ route('admin.product.store') }}" enctype="multipart/form-data">
                 @csrf
                 <div class="row">
                     <div class="col">
-                        <!-- Corrected: Removed misplaced 'row' attribute -->
-                        <div class="mb-3">
-                            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label"> Name: </label>
+                        <div class="mb-3 row">
+                            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Name:</label>
                             <div class="col-lg-10 col-md-6 col-sm-12">
                                 <input name="name" value="{{ old('name') }}" type="text" class="form-control">
                             </div>
                         </div>
                     </div>
                     <div class="col">
-                        <!-- Corrected: Removed misplaced 'row' attribute -->
-                        <div class="mb-3">
-                            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label"> Price: </label>
+                        <div class="mb-3 row">
+                            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Price:</label>
                             <div class="col-lg-10 col-md-6 col-sm-12">
                                 <input name="price" value="{{ old('price') }}" type="number" class="form-control">
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3 row">
+                            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Image:</label>
+                            <div class="col-lg-10 col-md-6 col-sm-12">
+                                <input class="form-control" type="file" name="image">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        &nbsp;
+                    </div>
+                </div>
                 <div class="mb-3">
-                    <label class="form-label"> Description </label>
+                    <label class="form-label">Description</label>
                     <textarea class="form-control" name="description" rows="3">{{ old('description') }}</textarea>
                 </div>
-                <button type="submit" class="btn btn-primary"> Submit </button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
 
-    {{-- Manage Products --}}
     <div class="card">
         <div class="card-header">
             Manage Products
         </div>
-
         <div class="card-body">
             <table class="table table-bordered table-striped">
                 <thead>
@@ -71,9 +73,8 @@
                 <tbody>
                     @foreach ($viewData['products'] as $product)
                         <tr>
-                            <!-- Corrected: Assuming you're using Eloquent, access properties directly -->
-                            <td>{{ $product->id }}</td>
-                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->getId() }}</td>
+                            <td>{{ $product->getName() }}</td>
                             <td>Edit</td>
                             <td>Delete</td>
                         </tr>
