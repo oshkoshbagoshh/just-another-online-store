38950e48e8112df7c554a59376e86a8d9577454c
AJ <amirjavadi25@gmail.com>
2023-12-21 16:16:16 -0600

    about to seed the database with fake users

---
+++ database/seeders/userstableseeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Faker\Factory as Faker;
+
+
+
+class userstableseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = Faker::create();
+
+        foreach(range(1,10) as $index){
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password,
+                'role' => $faker->role,
+                'remember_token' => $faker->remember_token,
+            ]);
+        }
+    }
+}
